cd5b0df730ac874953cea666d28d8ae1cb9863dc
abdisalanvp77 <abdisalanvp77@gmail.com>
2019-04-10 21:39:10 -0400

    made front-end changes

--- globalkitchenapp/models.py
+++ globalkitchenapp/models.py
@@ -15,7 +15,7 @@ class RegisteredUser(models.Model):
     def __str__(self):
         return self.userName
 class Booking(models.Model):
-    id = models.AutoField(primary_key=True, editable=False)
+    id = models.AutoField(primary_key=True, default=0, editable=False)
     eventName = models.CharField(max_length=200)
     bookerName = models.CharField(max_length=200)
     bookerEmailField = models.EmailField()
